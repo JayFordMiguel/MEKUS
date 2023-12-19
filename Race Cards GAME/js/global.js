5e668264e6ba1f25f2f223077db459220f91335b
JayFordMiguel <142868378+JayFordMiguel@users.noreply.github.com>
2023-12-19 10:09:25 +0800

    initial commit

---
+++ Race Cards GAME/js/global.js
@@ -0,0 +1,37 @@
+let modeRelax = false;
+let movement = 0;
+let stopwatch;
+let cardGroup = [
+    ["🛴","🦼"],
+    ["🛵","🏍️"],
+    ["🚙","🚗","🛻","🚐"],
+    ["🚚","🚔","🚍","🚝"],
+    ["🛺","🚕","🚑","🚓"]
+   
+
+    ];
+
+let levelActual = 0;
+let levels =[
+    {
+        cards:cardGroup[0],
+        movementMax: 4
+    },
+    {
+        cards:cardGroup[0].concat(cardGroup[1]),
+        movementMax: 10
+    },
+    {
+        cards:cardGroup[0].concat(cardGroup[1], cardGroup[2]),
+        movementMax: 14
+    },
+    {
+        cards:cardGroup[0].concat(cardGroup[1], cardGroup[2],cardGroup[3]),
+        movementMax: 25
+    },
+    {cards:cardGroup[0].concat(cardGroup[1],cardGroup[2],cardGroup[3],cardGroup[4]),
+    movementMax:60
+    
+    }
+
+];
